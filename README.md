548fec3a95c68f454ac5fae2a5d1311eddea4257
seaung <seaung.c0m7de@protonmail.com>
2020-09-15 23:35:54 +0800

    push code

---
+++ README.md
@@ -0,0 +1,22 @@
+#### RsSubdomain
+
+这是一个简单的，基于python3开发的，暴力破解子域名工具
+
+
+#### Usage
+
+使用也非常的简单
+
+
+```
+python3 RsSubdomain.py --filename domain.txt --domain baidu.com
+```
+
+
+#### 效果图
+
+![效果图](./rssubdomain.png)
+
+
+---
+that's all
--- RsSubdomain.py
+++ RsSubdomain.py
@@ -1,6 +1,7 @@
 import argparse
 import threading
 import sys
+import dns.resolver
 
 from queue import Queue
 
@@ -27,8 +28,7 @@ def _set_commandline_options():
     """
     设置命令行参数
     """
-    add_commandline_options = argparse.ArgumentParser("\r\n
-                                                      一个非常简单的子域名查询工具\r\n")
+    add_commandline_options = argparse.ArgumentParser("\n\r一个非常简单的子域名查询工具\n\r")
     add_commandline_options.add_argument("--domain", dest="domain",
                                          action="store", help="目标站点主域名.",
                                         type=str)
@@ -43,8 +43,7 @@ def _set_commandline_options():
 
 def print_usage_text():
     usage_text = """
-root@home~> python3 RsSubdomain.py --domain baidu.com
-root@home~> python3 RsSubdomain.py --filename ./domain.txt
+root@home~> python3 RsSubdomain.py --filename ./domain.txt --domain baidu.com
     """
 
 
@@ -54,20 +53,21 @@ class RsSubDomain(object):
         self.domain = domain
         self.thread_number = thread_number
         self.queue = Queue()
+        self.result = []
 
     def run(self):
-        if domain is None:
-            loader_queue = loader_file(self.filename)
-            for queue_item in loader_queue:
-                self.queue.put(queue_item)
-        else:
-            self.queue.put(self.domain)
+        #if self.domain is None:
+        loader_queue = loader_file(self.filename)
+        for queue_item in loader_queue:
+            self.queue.put(queue_item + "." + self.domain)
+        #else:
+            #self.queue.put(self.domain)
 
         total = self.queue.qsize()
         threads = []
 
         for number in range(self.thread_number):
-            threads.append(self.RsSubDomainThreading(self.queue, self.total))
+            threads.append(self.RsSubDomainThreading(self.queue, self.result, total))
 
         for th in threads:
             th.start()
@@ -75,23 +75,33 @@ class RsSubDomain(object):
         for th in threads:
             th.join()
 
+        return list(set(self.result))
+
     class RsSubDomainThreading(threading.Thread):
-        def __init__(self, queue, total):
-            super().__init__()
+        def __init__(self, queue, result, total):
+            #super(RsSubDomainThreading, self).__init__(queue, result, total)
+            threading.Thread.__init__(self)
             self.queue = queue
             self.total = total
+            self.result = result
 
         def run(self):
             while not self.queue.empty():
                 queue_element = self.queue.get_nowait()
-
-                self.progress_message()
+                try:
+                    self.progress()
+                    result = dns.resolver.query(queue_element, 'A')
+                    if result.response.answer:
+                        self.result.append(queue_element)
+                except Exception as e:
+                    pass
 
 
         def progress(self):
             done = float(self.total - self.queue.qsize())
             count = float(self.total)
-            progress_message = "[+] Last {} | complete progress {:.2f}".format(self.queue.qsize(), (done/count)*100)
+            found_count = len(self.result)
+            progress_message = "[+] Last {} | complete progress {:.2f} | Found {}".format(self.queue.qsize(), (done/count)*100, found_count)
             sys.stdout.write("\r" + progress_message)
             sys.stdout.flush()
 
@@ -99,18 +109,26 @@ class RsSubDomain(object):
 def main():
     parser, options = _set_commandline_options()
 
-    if (options.filename or options.domain) is None:
-        parser.print_help()
-        parser.print_usage()
+    if (parser.filename and parser.domain) is None:
+        options.print_help()
+        options.print_usage()
         print_usage_text()
         exit(0)
+
+    result = RsSubDomain(domain=parser.domain, filename=parser.filename).run()
+
+
+    print()
+
+    for item in result:
+        print("[*] DNS Record : " + item)
 
 
 def print_logo_text():
     logo_text = f"""
-        {__AUTHOR__}
+                            {__AUTHOR__}
 
-        {__VERSION__}
+                            {__VERSION__}
      ____        ____        _         _                       _
     |  _ \ ___  / ___| _   _| |__   __| | ___  _ __ ___   __ _(_)_ __
     | |_) / __| \___ \| | | | '_ \ / _` |/ _ \| '_ ` _ \ / _` | | '_
